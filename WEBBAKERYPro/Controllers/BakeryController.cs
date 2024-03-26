2014ff5c611ad546e4fb117efcc234bc5e866ac7
kythong <kythong2003@gmail.com>
2024-03-26 13:00:15 +0700

    update decorator by tky

--- WEBBAKERYPro/Controllers/BakeryController.cs
+++ WEBBAKERYPro/Controllers/BakeryController.cs
@@ -13,14 +13,5 @@ namespace WEBBAKERYPro.Controllers
         {
             return View();
         }
-        public ActionResult BakeryAccessory()
-        {
-            List<SelectListItem> list = new List<SelectListItem>();
-            list.Add(new SelectListItem { Text = "Set muỗng, đĩa, dao cắt bánh", Value = "0" });
-            list.Add(new SelectListItem { Text = "Nến", Value = "1" });
-            list.Add(new SelectListItem { Text = "Kẹo cốm", Value = "2" });
-            ViewBag.listAccessory = list;
-            return View();
-        }
     }
 }
